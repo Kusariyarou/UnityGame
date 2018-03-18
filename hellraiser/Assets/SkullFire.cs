541b52f20587416e8532f7f4bdacdfc2678c7bfe
Kusariyarou <demiralpcagdas@gmail.com>
2018-03-18 15:48:13 +0300

    Update 10

---
+++ hellraiser/Assets/SkullFire.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullFire : MonoBehaviour {
+
+	private Animator skullAnimator;
+
+	float timer;
+
+	int waitingTime = 5; 
+
+	// Use this for initialization
+	void Start () {
+		 
+		skullAnimator = GetComponent<Animator>();
+	}
+	
+	void Update()
+	{
+		timer += Time.deltaTime;
+		if (timer > waitingTime) 
+		{
+			skullAnimator.SetTrigger ("FireBall");
+			timer = 0;
+		}
+		
+	}
+
+
+
+
+}
